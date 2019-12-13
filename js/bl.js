fab0498fa53d0fda3ab7a8682ce1a89da83de244
vojto <vojto@awesomenet.eu>
2019-12-13 12:47:49 +0100

    Ajax request to api added.

--- js/bl.js
+++ js/bl.js
@@ -1,2 +1,47 @@
-var ctx = document.getElementById('myChart').getContext('2d');
+"use strict";
 
+class ApiService {
+    constructor(period) {
+        var self = this;
+        this.period = period | 1000;
+        this.http = http;
+        this.interval = setIntarval(function () {
+            self.obtainData();
+        }, period);
+        this.obtainData();
+    }
+
+    obtainData() {
+        var self = this;
+        $.ajax(
+            window.prefix + '/api',
+            {
+                success: function(data, textStatus, jqXHR) {
+                    self.getDataSuccessHandler(data);
+                },
+                error: function(jqXHR, textStatus, error) {
+                    self.getDataErrorHandler(error);
+                }
+            }
+        );
+    }
+
+    getDatasSuccessHandler(data) {
+        if (typeof data == 'string') {
+            window.location.reload();
+            return;
+        }
+        this.data.downloads = data;
+    }
+
+    getDataErrorHandler(error) {
+        console.log(error);
+        clearInterval(this.interval);
+    }
+}
+
+document.onload = function() {
+    let apiService = new ApiService(1000);
+
+    var ctx = document.getElementById('myChart').getContext('2d');
+};
--- systatusJson.py
+++ systatusJson.py
@@ -6,7 +6,17 @@ import psutil
 from psutil._common import bytes2human
 
 data = {}
-data["temperatures"] = psutil.sensors_temperatures()
-data["memory"] = psutil.virtual_memory()
+data['temperatures'] = {}
+for tmp, vals in psutil.sensors_temperatures().items():
+    data['temperatures'][tmp] = []
+    for i, val in enumerate(vals):
+         data['temperatures'][tmp].append({})
+         for j, item in enumerate(val):
+             data['temperatures'][tmp][i][val._fields[j]] = item
+
+data['memory'] = {}
+vm = psutil.virtual_memory()
+for i, value in enumerate(vm):
+    data['memory'][vm._fields[i]] = value
 
 print(json.dumps(data))
